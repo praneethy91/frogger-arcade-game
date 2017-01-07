5fa83cbd8fa77b9df1dcb26a12018488fa4ec519
praneethy91 <praneethy91@gmail.com>
2017-01-07 18:12:49 -0500

    Getting the player sprite and it's movement in, have not handled edge cases. Also filled up code for the enemy to show up, which right now is one enemy which is static

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
+    this.x = x;
+    this.y = y;
     this.sprite = 'images/enemy-bug.png';
 };
 
@@ -21,11 +23,44 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+var Player = function(x, y) {
+    this.x = x;
+    this.y = y;
+    this.sprite = 'images/char-boy.png';
+}
+Player.prototype.update = function(dt) {
+    // Do nothing, most of the updating for instance variables
+    // happens in the handleInput
+}
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+Player.prototype.handleInput = function(key) {
+    if(key === undefined) {
+        return;
+    }
+
+    if(key === 'left') {
+        this.x -= 101;
+    }
+    else if(key === 'right') {
+        this.x += 101;
+    }
+    else if(key === 'up') {
+        this.y -= 83;
+    }
+    else if(key === 'down') {
+        this.y += 83;
+    }
+}
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
-
+var allEnemies = [];
+var Enemy = new Enemy(0, 83);
+allEnemies.push(Enemy);
+player = new Player(202, 380);
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
