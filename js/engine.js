987e73b376947771492ab52f4c6baa34a7746ca4
praneethy91 <praneethy91@gmail.com>
2017-01-08 22:14:39 -0500

    Rendering the enemies after the gem, so that they pass over them

--- js/engine.js
+++ js/engine.js
@@ -173,14 +173,14 @@ var Engine = function(global) {
         /* Loop through all of the objects within the allEnemies array and call
          * the render function you have defined.
          */
+        gem.render();
+        heart.render();
         allEnemies.forEach(function(enemy) {
             enemy.render();
         });
 
         player.render();
         gameState.render();
-        gem.render();
-        heart.render();
     }
 
     /* This function does nothing but it could have been a good place to
